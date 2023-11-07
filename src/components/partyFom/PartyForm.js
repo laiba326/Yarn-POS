023c9d00c75474928e21ed4372b84d774bdbd7c7
laiba irfan <laibairfan938@gmail.com>
2023-11-07 12:02:41 +0500

    forms

--- src/components/partyFom/PartyForm.js
+++ src/components/partyFom/PartyForm.js
@@ -1,27 +1,42 @@
-import React, { useState } from "react";
-import { Option } from "antd/es/mentions";
-import { Button, Form, Input, Radio, Select } from "antd";
+import React from "react";
+import { Button, Form, Input, Select } from "antd";
+import styled from "styled-components";
 
-const ReactFc = () => {
-  const [componentSize, setComponentSize] = useState("default");
+const StyledForm = styled(Form)`
+  max-width: 600px;
+  margin: auto;
+  background-color: #f0f0f0;
+  padding: 20px;
+  margin-bottom: 800px;
+  box-shadow: 0 0 20px 10px rgba(0, 0, 0, 0.4);
+`;
+
+const PartyForm = () => {
   const prefixSelector = (
     <Form.Item name="prefix" noStyle>
-      <Select style={{ width: 70 }}>
-        <Option value="86">+86</Option>
-        <Option value="87">+87</Option>
+      <Select style={{ width: 50 }}>
+        <Select.Option value="92">+92</Select.Option>
       </Select>
     </Form.Item>
   );
+  const onFinish = (values) => {
+    console.log(values);
+  };
+
   return (
-    <Form
+    <StyledForm
       labelCol={{ span: 4 }}
       wrapperCol={{ span: 14 }}
       layout="horizontal"
-      initialValues={{ size: componentSize }}
       style={{ maxWidth: 600 }}
+      onFinish={onFinish}
     >
       <h1>Party Form</h1>
-      <Form.Item label="address">
+      <Form.Item label="Party Name" name="Party Name">
+        <Input />
+      </Form.Item>
+
+      <Form.Item label="Address" name="Address">
         <Input />
       </Form.Item>
 
@@ -33,22 +48,24 @@ const ReactFc = () => {
         <Input addonBefore={prefixSelector} style={{ width: "100%" }} />
       </Form.Item>
 
-      <Form.Item label="phone1">
+      <Form.Item label="Phone1" name="Phone1">
         <input />
       </Form.Item>
 
-      <Form.Item label="phone2">
+      <Form.Item label="Phone2" name="Phone2">
         <input />
       </Form.Item>
 
-      <Form.Item label="phone3">
+      <Form.Item label="Phone3" name="Phone3">
         <input />
       </Form.Item>
       <Form.Item label="Button">
-        <Button>Get Started</Button>
+        <Button type="primary" htmlType="submit">
+          Get Started
+        </Button>
       </Form.Item>
-    </Form>
+    </StyledForm>
   );
 };
 
-export default ReactFc;
+export default PartyForm;

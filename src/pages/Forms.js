8571fc7fad32be70167823bd49dde0bb8a2c45f7
Muhammad Shahzad <muhammad.shahzad@selectquote.com>
2023-11-23 12:37:59 +0500

    after adjusting dashboard css

--- src/pages/Forms.js
+++ src/pages/Forms.js
@@ -1,18 +1,18 @@
 import React from "react";
 import DashboardIcon from "../components/dashboardIcon/DashboardIcon";
 import { formElements } from "../configs/formElements";
+import { Row } from "antd";
+
 export default function Forms() {
   return (
-    <>
+    <Row>
       {formElements.map((element) => (
-        <>
-          <DashboardIcon
-            title={element.title}
-            icon={element.icon}
-            comp={element.comp}
-          />
-        </>
+        <DashboardIcon
+          title={element.title}
+          icon={element.icon}
+          comp={element.comp}
+        />
       ))}
-    </>
+    </Row>
   );
 }

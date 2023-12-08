280383fbfdc4314155e867fc082d25f7905d99fb
laiba irfan <laibairfan938@gmail.com>
2023-12-08 12:08:00 +0500

    party option slic

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import party from "../slice/partySlice";
-
+import partyOptionSlice from "../slice/partyOptionSlice";
 export default configureStore({
-  reducer: { party },
+  reducer: { party, partyOptionSlice },
 });
